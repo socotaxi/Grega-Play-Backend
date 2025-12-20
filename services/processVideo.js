fec57744db62c50e1d4c8fd4238aa0e8c16a00d7
socotaxi <socotaxi@gmail.com>
2025-12-20 10:56:11 +0100

    fix FFmpeg - yen a marre4

--- services/processVideo.js
+++ services/processVideo.js
@@ -791,12 +791,13 @@ async function normalizeVideo(
   const inputHasAudio = await hasAudioStream(inputPath);
 
   const vFilter =
-    `settb=AVTB,setpts=PTS-STARTPTS,` +
-    `fps=${fps},` +
-    `scale=720:1280:force_original_aspect_ratio=decrease,` +
-    `pad=720:1280:(ow-iw)/2:(oh-ih)/2:black,` +
-    `setsar=1,format=yuv420p`;
-
+  `settb=AVTB,` +
+  `fps=${fps},` +
+  `setpts=N/(${fps}*TB),` +
+  `scale=720:1280:force_original_aspect_ratio=decrease,` +
+  `pad=720:1280:(ow-iw)/2:(oh-ih)/2:black,` +
+  `setsar=1,format=yuv420p`;
+  
   const aFilter =
     `asetpts=PTS-STARTPTS,` +
     `aformat=sample_fmts=fltp:sample_rates=48000:channel_layouts=stereo,` +
@@ -906,7 +907,7 @@ async function runFFmpegFilterConcat(
   const parts = [];
   for (let i = 0; i < n; i++) {
     parts.push(
-      `[${i}:v]settb=AVTB,setpts=PTS-STARTPTS,fps=30,format=yuv420p,` +
+      `[${i}:v]settb=AVTB,fps=30,setpts=N/(30*TB),format=yuv420p,` +
         `scale=720:1280:force_original_aspect_ratio=decrease,` +
         `pad=720:1280:(ow-iw)/2:(oh-ih)/2:black,setsar=1[v${i}];` +
         `[${i}:a]asetpts=PTS-STARTPTS,` +
